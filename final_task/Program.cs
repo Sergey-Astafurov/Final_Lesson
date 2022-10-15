4f9364afab613bc3440e25bb2df1cc38b27f230e
Sergey-Astafurov <stupidsociety16@gmail.com>
2022-10-15 15:31:49 +0500

    написал решение задачи

--- final_task/Program.cs
+++ final_task/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// Метод который выбирает конкретные строки из массива
+string TestLenght(string[] arr)
+{
+   string newArr = ""; // создаем новую строку куда будем помещать отобраные строки из массива
+   for (int i = 0; i < arr.Length; i++)
+   { //пишем цикл
+      if (arr[i].Length <= 3)
+      { //условие чтобы все строки которые по длине меньше или равны трем записывались в новую переменную
+         newArr = newArr + arr[i] + " ";
+      }
+
+
+   }
+   return newArr; // возвращаем эту переменную
+}
+//метод вызова результата
+void PrintResult(string line)
+{
+   Console.WriteLine(line);
+}
+//тело программы
+string[] arr = { "1", "23", "455", "88889", "8937423", "5432", "11", "Hell", "Hi" };
+string test = TestLenght(arr);
+PrintResult(test);
